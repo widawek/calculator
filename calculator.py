8ca6c27d7fe46695bf21cfa605966bd30a7b9e16
widawek <kolczywiewicz@gmail.com>
2023-11-14 20:41:39 +0100

    Add inputs and calculator function

---
+++ calculator.py
@@ -0,0 +1,21 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+operation = int(input('Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie: '))
+num1 = int(input('Podaj pierwszą liczbę: '))
+num2 = int(input('Podaj drugą liczbę: '))
+
+
+def calculator(operation, num1, num2):
+    if operation == 1:
+        pass
+    if operation == 2:
+        pass
+    if operation == 3:
+        pass
+    if operation == 4:
+        pass
+
+
+if __name__ == '__main__':
+    calculator(operation)
